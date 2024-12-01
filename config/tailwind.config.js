60115bda17f6a9132d9aa4dc70e7fd52acbd46ce
Dan Burt <gromitzy@gmail.com>
2024-12-01 18:19:18 +0100

    tailwind install

---
+++ config/tailwind.config.js
@@ -0,0 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',       // All ERB templates
+    './app/helpers/**/*.rb',          // Helper files
+    './app/javascript/**/*.js',       // JavaScript files
+    './app/javascript/**/*.jsx',      // React (if applicable)
+    './app/assets/stylesheets/**/*.css', // Any CSS files
+    './app/components/**/*.{erb,html}',  // ViewComponent files (if using ViewComponents)
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
